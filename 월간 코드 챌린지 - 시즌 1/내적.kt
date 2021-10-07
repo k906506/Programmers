f299bbe60ed31b296e70fc8bc557bdee9f839d9d
Dohyeon Ko <kodo@kakao.com>
2021-10-07 21:31:50 +0900

    폴더 삭제

--- 월간 코드 챌린지 - 시즌 1/내적.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    var result = 0;
-    fun solution(a: IntArray, b: IntArray): Int {
-        for(i in a.indices){
-            result += a[i] * b[i];
-        }
-        return result
-    }
-}
--- 월간 코드 챌린지 - 시즌 1/내적.py
+++
@@ -1,6 +0,0 @@
-def solution(a, b):
-    answer = 0
-    for i in range(len(a)):
-        ans = a[i]*b[i]
-        answer += ans
-    return answer
