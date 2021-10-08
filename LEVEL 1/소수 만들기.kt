380e2901f2716f79db3af488111f137a70ff6987
Dohyeon Ko <kodo@kakao.com>
2021-10-08 14:21:08 +0900

    21.10.08 (Fri)

---
+++ LEVEL 1/소수 만들기.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun solution(nums: IntArray): Int {
+        var answer = 0
+        for (i in 0..nums.size - 3) {
+            for (j in i + 1..nums.size - 2) {
+                for (k in j + 1..nums.size - 1) {
+                    var temp : Int = nums[i] + nums[j] + nums[k]
+                    if (checkPrime(temp)) answer += 1
+                }
+                
+            }
+        }
+        return answer
+    }
+    
+    fun checkPrime(number : Int) : Boolean {
+        for (i in 2..number - 1) {
+            if (number % i == 0) {
+                return false
+            }
+        }
+        return true
+    }
+}
